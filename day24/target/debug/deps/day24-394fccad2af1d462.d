3ca377e07ced7e098e3693bb44893dbb7821483f
tommy <t.oosterwijk@flusso.nl>
2023-01-31 22:41:56 +0100

    Day24

---
+++ day24/target/debug/deps/day24-394fccad2af1d462.d
@@ -0,0 +1,5 @@
+/Users/tommy/AocRust/day24/target/debug/deps/day24-394fccad2af1d462.rmeta: src/main.rs
+
+/Users/tommy/AocRust/day24/target/debug/deps/day24-394fccad2af1d462.d: src/main.rs
+
+src/main.rs:
---
+++ day24/target/debug/deps/day24-ed9907f46f35c20c.d
@@ -0,0 +1,5 @@
+/Users/tommy/AocRust/day24/target/debug/deps/day24-ed9907f46f35c20c.rmeta: src/main.rs
+
+/Users/tommy/AocRust/day24/target/debug/deps/day24-ed9907f46f35c20c.d: src/main.rs
+
+src/main.rs:
